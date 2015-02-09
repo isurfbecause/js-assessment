6ede710fc02abccbf32ce4b0d28972143d07ebe0
winston_yee <winsyee@gmail.com>
2015-02-08 21:48:29 -0800

    Complete objects section

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+        var result = fn.call(obj);
+        return result;
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+        var result = [];
+        for(var prop in obj) {
+            if(obj.hasOwnProperty(prop)) {
+            result.push(prop + ': ' + obj[prop]);
+            }
+        }
+        return result;
     }
   };
 });
