fa88f1fefb6ec9f7b29c0a79ebfa214b5d226d40
winston_yee <winsyee@gmail.com>
2015-02-08 23:01:43 -0800

    Complete flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -4,13 +4,27 @@ define(function() {
   return {
     fizzBuzz : function(num) {
       // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+      if(num % 15 === 0) {
+        // if the number is divisible by 3 and 5, the function should return
+        // 'fizzbuzz';
+        return 'fizzbuzz';
+      }
+      else if(num % 5 === 0) {
+        // if the number is divisible by 5, the function should return 'buzz';
+        return 'buzz';
+      }
+      else if(num % 3 === 0) {
+        // if the number is divisible by 3, the function should return 'fizz';
+        return 'fizz';
+      } else {
+        // was provided or the value provided is not a number
+        if(isNaN(num)) {
+          return false;
+        } else {
+          // otherwise the function should return the number, or false if no number
+          return num;
+        }
+      }
     }
   };
 });
